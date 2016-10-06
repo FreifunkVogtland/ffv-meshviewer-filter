91976e29ed0d0832cc528ec75b90ce16ffebef73
Sven Eckelmann <sven@narfation.org>
2016-10-06 17:01:11 +0200

    Add hack to have faster timeout on FFC meshviewer instance

--- filter.py
+++ filter.py
@@ -11,96 +11,6 @@ def dump_json(data, filename):
 		f.flush()
 		os.fsync(f.fileno())
 
-def valid_node(n):
-	# is ffv firmware?
-	if n['nodeinfo']['software']['firmware']['release'].endswith('-v'):
-		return True
-
-	# is vpn?
-	if 'vpn' in n['nodeinfo'] and n['nodeinfo']['vpn']:
-		return True
-
-	return False
-
-def get_nodes_validity(nodes):
-	return {n['nodeinfo']['node_id']: valid_node(n) for n in nodes['nodes']}
-
-def drop_contact_info(n):
-	if 'owner' in n:
-		del(n['owner'])
-	return n
-
-def filter_nodes(nodes, valid_nodes):
-	# filter contact info due to privacy concerns
-	nodes_priv = map(drop_contact_info, nodes['nodes'])
-
-	# only save valid nodes
-	nodes_ffv = filter(lambda n: valid_nodes[n['nodeinfo']['node_id']], nodes_priv)
-
-	nodes['nodes'] = list(nodes_ffv)
-
-	# remove owner information
-	for n in nodes['nodes']:
-		if not 'nodeinfo' in n:
-			continue
-
-		if not 'owner' in n['nodeinfo']:
-			continue
-
-		del n['nodeinfo']['owner']
-
-def filter_graph(graph, valid_nodes):
-	orig_pos = 0
-	new_pos = 0
-	pos_map = {}
-
-	# calculate new list of nodes but first create mapping for old to new node index
-	for n in graph['batadv']['nodes']:
-		if 'node_id' in n and n['node_id'] in valid_nodes and valid_nodes[n['node_id']]:
-			pos_map[orig_pos] = new_pos
-			new_pos += 1
-		orig_pos += 1
-
-	nodes_ffv = filter(lambda n:'node_id' in n and n['node_id'] in valid_nodes and valid_nodes[n['node_id']], graph['batadv']['nodes'])
-	graph['batadv']['nodes'] = list(nodes_ffv)
-
-	# filter links with their new ids
-	new_links = []
-	for l in graph['batadv']['links']:
-		if not l['source'] in pos_map:
-			continue
-
-		if not l['target'] in pos_map:
-			continue
-
-		l['source'] = pos_map[l['source']]
-		l['target'] = pos_map[l['target']]
-
-		new_links.append(l)
-
-	graph['batadv']['links'] = new_links
-
-def filter_nodelist(nodelist, valid_nodes):
-	nodes_ffv = filter(lambda n: n['id'] in valid_nodes and valid_nodes[n['id']], nodelist['nodes'])
-
-	nodelist['nodes'] = list(nodes_ffv)
-
-def filter_json(graph, nodes, nodelist):
-	valid_nodes = get_nodes_validity(nodes)
-
-	# force server brewster with old ffnord-alfred-announce to be part of FFV
-	valid_nodes['7446a06579d2'] = True
-
-	# force server newton with old ffnord-alfred-announce to be part of FFV
-	valid_nodes['001d92340f69'] = True
-
-	# force server pascal with old ffnord-alfred-announce to be part of FFV
-	valid_nodes['e89a8f508b7b'] = True
-
-	filter_nodes(nodes, valid_nodes)
-	filter_graph(graph, valid_nodes)
-	filter_nodelist(nodelist, valid_nodes)
-
 def main():
 	if len(sys.argv) != 3:
 		print("./filter INPATH OUTPATH")
@@ -109,31 +19,40 @@ def main():
 	inpath = sys.argv[1]
 	outpath = sys.argv[2]
 
-	graph_in = os.path.join(inpath, "graph.json")
-	graph_out = os.path.join(outpath, "graph.json")
-	graph_outtmp = os.path.join(outpath, "graph.json.tmp")
 	nodes_in = os.path.join(inpath, "nodes.json")
 	nodes_out = os.path.join(outpath, "nodes.json")
 	nodes_outtmp = os.path.join(outpath, "nodes.json.tmp")
-	nodelist_in = os.path.join(inpath, "nodelist.json")
-	nodelist_out = os.path.join(outpath, "nodelist.json")
-	nodelist_outtmp = os.path.join(outpath, "nodelist.json.tmp")
 
 	# load
-	graph = json.load(open(graph_in))
 	nodes = json.load(open(nodes_in))
-	nodelist = json.load(open(nodelist_in))
 
-	filter_json(graph, nodes, nodelist)
+	for n in nodes['nodes']:
+		if not 'nodeinfo' in n:
+			continue
+
+		nodeinfo = n['nodeinfo']
+
+		if not 'software' in nodeinfo:
+			continue
+
+		software = nodeinfo['software']
+		if not 'firmware' in software:
+			continue
+
+		firmware = software['firmware']
+
+		if not 'release' in firmware:
+			continue
+
+		if not firmware['release'].endswith('-v'):
+			continue
+
+		n['lastseen'] = '2016-09-04T23:35:02'
 
 	# save
-	dump_json(graph, graph_outtmp)
 	dump_json(nodes, nodes_outtmp)
-	dump_json(nodelist, nodelist_outtmp)
 
-	os.rename(graph_outtmp, graph_out)
 	os.rename(nodes_outtmp, nodes_out)
-	os.rename(nodelist_outtmp, nodelist_out)
 
 if __name__ == "__main__":
 	main()
