2a9923e52de1ac1719f6d7577d65e6f97a1f0612
Sven Eckelmann <sven@narfation.org>
2018-01-03 10:35:36 +0100

    ffv-meshviewer-filter: Don't add FFC server anymore to valid nodes
    These servers are not anymore in the same network. It can therefore be
    dropped.

--- filter.py
+++ filter.py
@@ -361,15 +361,6 @@ def generate_wireless_stats(nodes):
 def filter_json(graph, nodes, nodelist, meshviewer):
 	valid_nodes = get_nodes_validity(nodes)
 
-	# force server brewster with old ffnord-alfred-announce to be part of FFV
-	valid_nodes['7446a06579d2'] = True
-
-	# force server newton with old ffnord-alfred-announce to be part of FFV
-	valid_nodes['001d92340f69'] = True
-
-	# force server pascal with old ffnord-alfred-announce to be part of FFV
-	valid_nodes['e89a8f508b7b'] = True
-
 	filter_nodes(nodes, valid_nodes)
 	filter_graph(graph, valid_nodes)
 	filter_meshviewer(meshviewer, valid_nodes)
